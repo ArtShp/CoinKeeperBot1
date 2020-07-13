eea3c568b3a4d3d4290e949cff14507ebc4b7985
Артём <56469391+ArtShp@users.noreply.github.com>
2020-07-13 19:51:03 +0300

    Adding categories class

---
+++ categories.py
@@ -0,0 +1,37 @@
+from typing import Dict, List
+
+import db
+
+
+class Categories:
+    def __init__(self) -> List[Dict]:
+        self._categories = self._load_categories()
+
+    def _load_categories(self) -> List[Dict]:
+        categories = db.fetch_all('category', 'codename name is_base_expence aliases'.split())
+        self._fill_aliases(categories)
+        return categories
+
+    def _fill_aliases(self, categories) -> List[Dict]:
+        for index, category in enumerate(categories):
+            aliases = category['aliases'].split(',')
+            aliases = list(filter(None, map(str.strip, aliases)))
+            aliases.append(category['codename'])
+            aliases.append(category['name'])
+            categories[index]['aliases'] = aliases
+
+    def get_all_categories(self) -> List[Dict]:
+        return self._categories
+
+    def get_category(self, cn: str) -> str:
+        find = None
+        other_category = None
+        for category in self._categories:
+            if category['codename'] == 'other':
+                other_category = category
+            for alias in category['aliases']:
+                if cn in alias:
+                    find = category
+        if not find:
+            find = other_category
+        return find

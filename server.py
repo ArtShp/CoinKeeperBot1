8ec976a12e6a03bbb1a8d0a13eac71fcedf2ae9a
Артём <56469391+ArtShp@users.noreply.github.com>
2020-07-13 19:27:04 +0300

    Adding exception and expences module

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import os
 import logging
 import codecs
+import expences
+import exception
 
 from aiogram import Bot, Dispatcher, executor, types
 from jinja2 import Template
@@ -18,3 +20,20 @@ async def start_message(message: types.Message):
         template = Template(file.read())
         await message.reply(template.render(username=message.chat.username),
                                             parse_mode='HTML', reply=False)
+
+
+@dp.message_handler()
+async def add_expence(message: types.Message):
+    try:
+        expence = expences.add_expence(message.text)
+    except exception.NotCorrectMessage as e:
+        await message.reply(str(e), reply=False)
+        return
+    answer_message = (
+        f'Добавлены траты {expence.amount} рублей на {expence.category_name}\n\n'
+        f'Здесь выведем статистику за сегодня'
+    )
+    await message.reply(answer_message, reply=False)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
